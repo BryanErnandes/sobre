92923bc4f2c89770eb2ed6470eecefdc1d7c1af7
Bryan Williams <Bryandeduartina1999@outlook.com>
2022-02-17 11:09:17 -0300

    sobre

---
+++ js/FormularioCadastro.js
@@ -0,0 +1,47 @@
+Vue.directive('mask', VueMask.VueMaskDirective);
+
+const App = new Vue({
+    el: '#FormularioCadastro',
+    data() {
+        return {
+            cep: null,
+            data: null,
+            messageCep: null
+        }
+
+    },
+    methods: {
+        searchCep() {
+            console.log(this.cep.length)
+            if (this.cep.length == 8) {
+                fetch(`https://viacep.com.br/ws/${ this.cep }/json/`)
+                    .then(response => response.json())
+                    .then(data => this.data = data)
+                    .catch(error => console.log(error))
+            }
+        }
+    }
+})
+
+/*const App = {
+    data() {
+        return {
+            cep: null,
+            data: null,
+            messageCep: null
+        }
+
+    },
+    methods: {
+        searchCep() {
+            console.log(this.cep.length)
+            if (this.cep.length == 8) {
+                fetch(`https://viacep.com.br/ws/${ this.cep }/json/`)
+                    .then(response => response.json())
+                    .then(data => this.data = data)
+                    .catch(error => console.log(error))
+            }
+        }
+    }
+}
+Vue.createApp(App).mount('#FormularioCadastro');*/
---
+++ js/ObjetoPropriedades.js
@@ -0,0 +1,100 @@
+var Cliente = {
+    foto: 12345678,
+    nome: "Bruno Henrique",
+    sobrenome: "Da Silva",
+    telefone: "(14) 997983845",
+    cpf: "123.456.789-10",
+    DataNascimento: "1994/08/05",
+    sexo: "Masculino",
+    endereço: {
+        rua: "Lucia Boni São Pedro n°1-58",
+        bairro: "Nova Bauru",
+        complemento: "",
+        estado: "São Paulo",
+        cidade: "bauru",
+        cep: 17022 - 152,
+    }
+
+
+
+}
+
+var chopp = {
+    nome: "Brahma Claro",
+    marca: "Brahma",
+    harmonizacao: "Carnes e queijos leves, amendoins e comida de boteco.",
+    ibu: 10,
+    alcool: 4.5,
+    temperatura: "2ºC e 4ºC",
+    apresentacoes: [{
+            titulo1: "Barril de Chopp 10L",
+            preco1: 1
+        },
+        {
+            titulo2: "Barril de Chopp 15L",
+            preco2: 1
+        },
+        {
+            titulo3: "Barril de Chopp 20L",
+            preco3: 1
+        },
+        {
+            titulo4: "Garrafa 355ml",
+            preco4: 1
+        },
+        {
+            titulo5: "Garrafa 600ml",
+            preco5: 1
+        },
+        {
+            titulo6: "Growler 1L",
+            preco6: 1
+        },
+        {
+            titulo7: "Growler 1,5L",
+            preco7: 1
+        },
+        {
+            titulo8: "Lata 710ml",
+            preco8: 1
+        },
+    ],
+    avaliação: 4.5,
+}
+
+var Facebook = {
+    titulo: "Facebook",
+    url: "https://facebook.com/villadochoppbauru/",
+}
+
+var Instagram = {
+    titulo: "Instagram",
+    url: "https://instagram.com/villadochoppbauru/ ",
+}
+
+var Email = {
+    titulo: "Gmail",
+    url: "villadochoppbauru@gmail.com",
+}
+var Evento = {
+    titulo: "",
+    foto: "123456789",
+    data: "2022/02/07",
+    descrição: "",
+    localizaçao: "Av. Affonso José Aiello, 14-45 - Vila Aviação, Bauru - SP, CEP: 17018-520",
+}
+
+var Comentario = {
+    foto: "123456789",
+    nome: "Bruno Henrique da Silva",
+    avaliaçao: 4.5,
+    comentarios: "Lugar Muuito Agradável, Atendimento Nota 10",
+}
+
+var Galeria = {
+    titulo: "Brahama Black",
+    foto: "123456789",
+    data: "2022/02/07",
+    descrição: "",
+    localizaçao: "Av. Affonso José Aiello, 14-45 - Vila Aviação, Bauru - SP, CEP:17018-520",
+}
